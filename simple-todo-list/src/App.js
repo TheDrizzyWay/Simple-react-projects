5d6efe7e0def6f2ddb99a4bfd7eb43b3a802c07a
Christopher <christopher.iliyas@andela.com>
2019-03-13 17:32:45 +0100

    add hardcoded todos

--- simple-todo-list/src/App.js
+++ simple-todo-list/src/App.js
@@ -3,10 +3,41 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Learn React.',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Eat Lunch.',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Try not to go crazy.',
+        completed: false,
+      }
+    ]
+  }
+
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}/>
       </div>
     );
   }
---
+++ simple-todo-list/src/components/TodoItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+class TodoItem extends Component {
+  toggleStyle = () => {
+    return {
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div className="todo-div" style={this.toggleStyle()}>
+        <p>
+        <input type="checkbox" onChange={this.props.toggleComplete.bind(this, id)}/>
+        {' '}
+        { title }
+        </p>
+      </div>
+    );
+  }
+}
+
+//PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+export default TodoItem;
