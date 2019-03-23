6a9ce2825cefe17facecb96534831407ce5f7c39
Christopher Iliyas <christopher.iliyas@andela.com>
2019-03-23 02:06:38 +0100

    finish live display

--- live-crypto-display/src/components/CryptoCard.jsx
+++ live-crypto-display/src/components/CryptoCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CryptoCard extends Component {
   constructor(props) {
@@ -6,17 +7,49 @@ class CryptoCard extends Component {
 
     this.state = {
       name: props.name,
-      symbol: props.symbol
+      symbol: props.symbol,
+      price: null,
+      lastPrice: null,
     }
   }
 
+  pollPrice = () => {
+    const { symbol } = this.state;
+    axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=${symbol},USD`)
+      .then(res => res.data)
+      .then(data => {
+        this.setState((prevState) => ({
+          price: data.USD,
+          lastPrice: prevState.price !== data.USD ? prevState.price : prevState.lastPrice
+        }))
+      })
+      .catch(err => console.log(err));
+  }
+
+  priceChange = (lastPrice, price) => {
+    const difference = price - lastPrice;
+    const change = (difference/lastPrice) * 100;
+    return change.toFixed(4);
+  }
+
+  componentDidMount() {
+    this.pollPrice();
+    setInterval(this.pollPrice, 10000);
+  }
+
   render() {
+    const { name, symbol, price, lastPrice } = this.state;
+    const gainloss = lastPrice > price ? 'loss' : 'gain';
     return (
-      <div className="card">
+      <div className={`card ${gainloss}`}>
         <div className="name">
-        {this.state.name}
-        <span>({this.state.symbol})</span>
+          {name}
+          <span>({symbol})</span>
         </div>
+        <div className="percent">
+          {this.priceChange(lastPrice, price)}%
+        </div>
+        <div className="price">{price}</div>
       </div>
     );
   }
