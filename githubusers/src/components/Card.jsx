e67683e3a44872fd6c6c9593642108f6569eb62f
Christopher Iliyas <christopher.iliyas@andela.com>
2019-04-21 20:23:13 +0100

    fix class typo

--- githubusers/src/components/Card.jsx
+++ githubusers/src/components/Card.jsx
@@ -4,10 +4,11 @@ import '../App.css';
 const Card = props => {
   return (
     <div className="main-card">
-      <img alt="avatar" class="card-image" src={props.avatar_url}/>
+      <img alt="avatar" className="card-image" src={props.avatar_url}/>
       <div>
         <div className="card-name">{props.name}</div>
         <div>{props.bio}</div>
+        <div>{props.public_repos} Repositories</div>
       </div>
     </div>
   )
