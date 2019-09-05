c4b0a2481aa68f39d1150ffecc9b6850e859cdad
Christopher Iliyas <christopher.iliyas@andela.com>
2019-09-05 11:04:53 +0100

    fix socket id config

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ A couple of simple react apps built in a bid to learn the basics of React js
 4. Live updating crypto value card
 5. A simple app for displaying github users info using React Hooks from 'The React Handbook' by flaviocopes
 6. A reusable modal component using portals based on a tutorial by Tucker Blackwell
+7. Github authentication using socket.io
--- social-auth/client/src/components/Oauth.jsx
+++ social-auth/client/src/components/Oauth.jsx
@@ -34,7 +34,7 @@ const Oauth = (props) => {
         const width = 600, height = 600;
         const left = (window.innerWidth / 2) - (width / 2);
         const top = (window.innerHeight / 2) - (height / 2);
-        const url = `${apiUrl}/${provider}?socketId=${socket.ids}`;
+        const url = `${apiUrl}/${provider}?socketId=${socket.id}`;
     
         return window.open(url, '',       
           `toolbar=no, location=no, directories=no, status=no, menubar=no, 
@@ -66,7 +66,7 @@ const Oauth = (props) => {
               <FontAwesome
                 name={'times-circle'}
                 className={'close'}
-                onClick={this.closeCard.bind(this)}
+                onClick={closeCard}
               />
               <h4>{name}</h4>
             </div>
