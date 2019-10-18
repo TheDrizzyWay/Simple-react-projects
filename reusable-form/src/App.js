e689ccb70fa1dafac348254c03ab4621229457ba
Christopher Iliyas <christopheriliyas@gmail.com>
2019-10-19 00:39:56 +0100

    add error boundary

--- reusable-form/src/App.js
+++ reusable-form/src/App.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ErrorBoundary from './containers/ErrorBoundary';
 import { registerForm } from './formFields';
 import './App.css';
 
@@ -7,25 +8,27 @@ const Home = lazy(() => import('./containers/Home'));
 const SampleForm = lazy(() => import('./containers/SampleForm'));
 const App = () => (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/register"
-              render={() => (
-                <SampleForm
-                  action="register"
-                  formTitle="Registration"
-                  buttonText="Register"
-                  formStructure={registerForm}
-                />
-              )}
-            />
-          </Switch>
-        </Router>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route
+                exact
+                path="/register"
+                render={() => (
+                  <SampleForm
+                    action="register"
+                    formTitle="Registration"
+                    buttonText="Register"
+                    formStructure={registerForm}
+                  />
+                )}
+              />
+            </Switch>
+          </Router>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 
